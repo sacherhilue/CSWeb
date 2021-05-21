5178114ce53f676636fca24bb530fb45f5ddb6bb
sacherhilue <zhongshanyunwo@126.com>
2021-05-21 19:52:20 +0800

    增加物理模型

--- src/main/java/cn/edu/cqu/domains/User.java
+++ src/main/java/cn/edu/cqu/domains/User.java
@@ -2,15 +2,9 @@ package cn.edu.cqu.domains;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="UserTest")
@@ -22,35 +16,10 @@ public class User implements Serializable
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GenericGenerator(name="systemUUID",strategy="uuid")
-	@GeneratedValue(generator="systemUUID")
 	private String userID;
 	private String userName;
 	private String userGender;
 	private int age;
-	
-	@OneToMany(cascade=CascadeType.ALL,mappedBy = "user",fetch=FetchType.EAGER/*,orphanRemoval=true*/)
-//	private Set<Eat> userEat;
-//	
-//	@OneToMany(cascade=CascadeType.ALL,mappedBy = "user",fetch=FetchType.EAGER)
-//	private Set<UserFeedback> userFeedback;
-	
-//	public Set<Eat> getUserEat()
-//	{
-//		return userEat;
-//	}
-//	public void setUserEat(Set<Eat> userEat)
-//	{
-//		this.userEat = userEat;
-//	}
-//	public Set<UserFeedback> getUserFeedback()
-//	{
-//		return userFeedback;
-//	}
-//	public void setUserFeedback(Set<UserFeedback> userFeedback)
-//	{
-//		this.userFeedback = userFeedback;
-//	}
 	public String getUserName()
 	{
 		return userName;
